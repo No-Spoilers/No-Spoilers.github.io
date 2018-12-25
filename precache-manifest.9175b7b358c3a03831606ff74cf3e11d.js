be6f5950e33fb4792e3a8159794561f3de960223
Geoffrey Emerson <geoffrey.emerson.git@gmail.com>
2018-12-25 11:19:28 -0800

    test build

---
+++ precache-manifest.9175b7b358c3a03831606ff74cf3e11d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7f6f769e66418e5d5d6b",
+    "url": "/static/js/main.7f6f769e.chunk.js"
+  },
+  {
+    "revision": "afc6a1fd55696d12f0de",
+    "url": "/static/js/1.afc6a1fd.chunk.js"
+  },
+  {
+    "revision": "7f6f769e66418e5d5d6b",
+    "url": "/static/css/main.352d6039.chunk.css"
+  },
+  {
+    "revision": "1e7cb3e8dfa4b0ef7b56de228db83aab",
+    "url": "/index.html"
+  }
+];
